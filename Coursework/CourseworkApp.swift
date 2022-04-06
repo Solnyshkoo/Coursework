6818c014041c8049ba41aae8a19cb5391222e29c
Solnyshkoo <kepetrova@edu.hse.ru>
2022-04-06 17:37:36 +0300

    Initial Commit

---
+++ Coursework/CourseworkApp.swift
@@ -0,0 +1,17 @@
+//
+//  CourseworkApp.swift
+//  Coursework
+//
+//  Created by Ksenia Petrova on 06.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct CourseworkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
